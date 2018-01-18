68c1c5ef64a0dbff19ead63df4133927008e1618
solomon <solomon.neuwirt@kearsarge.org>
2018-01-18 13:54:45 -0500

    Pacboi is in the game

--- Pac.py
+++ Pac.py
@@ -1,5 +1,6 @@
 import pygame, sys, math, random
 from Points import *
+from Pacboi import *
 
 pygame.init()
 
@@ -13,14 +14,37 @@ bgColor = [r, g, b] = [18, 5, 180]
 points = [Points("sb", [100, 100]),
           Points("beat", [200, 200])]
 
+boi = Pacboi(size)
+
 level = 1
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                boi.go("up")
+            if event.key == pygame.K_DOWN:
+                boi.go("down")
+            if event.key == pygame.K_LEFT: 
+                boi.go("left")
+            if event.key == pygame.K_RIGHT: 
+                boi.go("right")
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP: 
+                boi.go("stop up")
+            if event.key == pygame.K_DOWN: 
+                boi.go("stop down")
+            if event.key == pygame.K_LEFT: 
+                boi.go("stop left")
+            if event.key == pygame.K_RIGHT: 
+                boi.go("stop right")
+             
+    boi.move()
             
     screen.fill(bgColor)
     for p in points:
         screen.blit(p.image, p.rect)
+    screen.blit(boi.image, boi.rect)
     pygame.display.flip()
     clock.tick(60)
---
+++ Pacboi.py
@@ -0,0 +1,48 @@
+import pygame, math
+
+class Pacboi():
+    def __init__(self, screenSize):
+        self.images=[pygame.image.load("Pacboi\Pacboi Closed.png"),
+                     pygame.image.load("Pacboi\Pacboi Open.png")]
+        self.frame = 0
+        self.maxFrame = len(self.images)-1
+        self.count = 0
+        self.countMax = 60/4
+        self.image = self.images[self.frame]
+        self.rect = self.image.get_rect()
+        self.maxSpeed = 6
+        self.speed = [0,0]
+          
+    def move(self):
+        self.animate()
+        self.rect.move_ip(self.speed)
+
+    def animate(self):
+        if self.count < self.countMax:
+            self.count += 1
+        else:
+            self.count = 0
+            if self.frame < self.maxFrame:
+                self.frame += 1
+            else:
+                self.frame = 0
+            self.image = self.images[self.frame]
+
+    def go(self, direction):
+        if direction == "up":
+            self.speed[1] = -self.maxSpeed
+        elif direction == "down":
+            self.speed[1] = self.maxSpeed
+        elif direction == "right":
+            self.speed[0] = self.maxSpeed
+        elif direction == "left":
+            self.speed[0] = -self.maxSpeed
+
+        if direction == "stop up":
+            self.speed[1] = 0
+        elif direction == "stop down":
+            self.speed[1] = 0
+        elif direction == "stop right":
+            self.speed[0] = 0
+        elif direction == "stop left":
+            self.speed[0] = 0
